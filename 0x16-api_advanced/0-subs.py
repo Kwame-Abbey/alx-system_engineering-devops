f950a41c2bf4e891770c5305da1374291f0871b1
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-03-05 16:18:22 +0000

    fix little bugs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Defines a function that queries the reddit API"""
+"""Defines a function that queries the reddit API
+"""
 
 import requests
 
@@ -9,11 +10,10 @@ def number_of_subscribers(subreddit):
     total subscribers) for a given subreddit
     """
     subreddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Dry-Lingonberry7149'}
-    try:
-        response = requests.get(subreddit_url, headers=headers,
+    headers = {'User-Agent': 'Abbey'}
+    response = requests.get(subreddit_url, headers=headers,
                                 allow_redirects=False).json()
-
+    try:
         return response.get('data').get('subscribers')
     except Exception:
         return 0
